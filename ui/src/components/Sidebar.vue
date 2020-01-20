495aa4b24ecc4a5fa313005c464de3da3906dd52
Matheus Manoel <matheuscmanoel@gmail.com>
2020-01-20 19:54:03 -0200

    Try add icon.

--- ui/src/components/Sidebar.vue
+++ ui/src/components/Sidebar.vue
@@ -5,69 +5,50 @@
     </div>
     <b-menu>
       <b-menu-list>
+        <span class="icon-home" />
+        <b-icon pack="redonicons" icon="icon-home"></b-icon>
+        <b-menu-item icon-pack="redonicons" icon="icon-home" label="Info"></b-menu-item>
+      </b-menu-list>
+      <b-menu-list label="Biblioteca">
         <b-menu-item icon="information-outline" label="Info"></b-menu-item>
-        <b-menu-item
-          icon="settings"
-          :active="isActive"
-          :expanded="isActive"
-          @click="isActive = !isActive">
-          <template slot="label" slot-scope="props">
-            Administrator
-            <b-icon
-              class="is-pulled-right"
-              :icon="props.expanded ? 'menu-down' : 'menu-up'">
-            </b-icon>
-          </template>
-          <b-menu-item icon="account" label="Users"></b-menu-item>
-          <b-menu-item icon="cellphone-link">
-            <template slot="label" slot-scope="{}">
-              Devices
-              <b-dropdown aria-role="list" class="is-pulled-right" position="is-bottom-left">
-                <b-icon icon="dots-vertical" slot="trigger"></b-icon>
-                <b-dropdown-item aria-role="listitem">Action</b-dropdown-item>
-                <b-dropdown-item aria-role="listitem">Another action</b-dropdown-item>
-                <b-dropdown-item aria-role="listitem">Something else</b-dropdown-item>
-              </b-dropdown>
-            </template>
-          </b-menu-item>
-          <b-menu-item icon="cash-multiple" label="Payments" disabled></b-menu-item>
-        </b-menu-item>
-        <b-menu-item icon="account" label="My Account">
-          <b-menu-item label="Account data"></b-menu-item>
-          <b-menu-item label="Addresses"></b-menu-item>
-        </b-menu-item>
-      </b-menu-list>
-      <b-menu-list>
-        <b-menu-item
-          label="Expo"
-          icon="link"
-          tag="router-link"
-          target="_blank"
-          to="/expo">
-        </b-menu-item>
-      </b-menu-list>
-      <b-menu-list label="Actions">
-        <b-menu-item label="Logout"></b-menu-item>
       </b-menu-list>
     </b-menu>
   </section>
 </template>
 
 <script>
+const customIconConfig = {
+  customIconPacks: {
+    'redonicons': {
+      sizes: {
+        'default': 'is-size-5',
+        'is-small': '',
+        'is-medium': 'is-size-3',
+        'is-large': 'is-size-1'
+      },
+      internalIcons: {
+        'icon-home': 'icon-home',
+      }
+    }
+  }
+}
+
 export default {
   name: 'Sidebar',
-  data() {
-    return {
-      isActive: true
-    }
-  },
+  created() {
+    this.$buefy.config.setOptions(customIconConfig)
+  }
 }
 </script>
+
+<style scoped>
+@import "https://file.myfontastic.com/SBqPSxgJ9ZpScWYRpfw4ga/icons.css";
+</style>
 
 <style>
 .logo-container {
   background: transparent linear-gradient(114deg, #D9000F 0%, #EA5703 51%, #EBAE1D 100%) 0% 0% no-repeat padding-box;
-opacity: 1;
+  opacity: 1;
   display: flex;
   justify-content: center;
 }
